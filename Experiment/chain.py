018c185dcd492db9ee5e0640f2e7c415fcec60f3
Ahmad-Fahad <ahmadsmfahad@gmail.com>
2020-11-19 21:28:56 +0600

    few update

--- Experiment/chain.py
+++ Experiment/chain.py
@@ -1,9 +1,11 @@
-chain = [1]
+chain = [[1]]
 
-def add_block():
-	chain.append([chain[0], 5.4])
+def add_block(transaction_amount):
+	chain.append([chain[-1], transaction_amount])
 	print(chain)
+	#print(chain[-1])
 
-add_block()
-add_block()
-add_block()
+add_block(2)
+add_block(3.9)
+add_block(4.9)
+
